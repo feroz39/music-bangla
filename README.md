d51acad3c97fe9768fd1a0e199beffa75ab40251
Md Feroz Hossain <mdferozhossain39@hotmail.com>
2019-02-01 19:04:27 +0600

    Gulp setup and init the env

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# music-bangla
+# music-bangla (The Design)
 MusicBangla is a music website for Bengali language.
+
+Everything is the git repo is the design part of the project.
---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+
+gulp.task('sass', ()=>
+    gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/sass/style.scss'])
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('src/css'))
+);
+
+gulp.task('concat', ()=>
+        gulp.src(
+            [
+                'node_modules/jquery/dist/jquery.min.js',
+                'node_modules/popper.js/dist/umd/popper.min.js',
+                'node_modules/bootstrap/dist/js/bootstrap.min.js',
+            ])
+        .pipe(concat('vendor.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('src/js/'))
+);
+
+gulp.task('sassWatch', function () {
+    gulp.watch(['src/sass/*.scss', 'src/sass/**/*.scss'], gulp.series('sass'));
+});
+
+gulp.task('default', gulp.series('sass', 'concat', 'sassWatch'));
